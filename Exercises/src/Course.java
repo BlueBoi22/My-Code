b047fb59011b858e603476cfe90e30cb94076f8b
Franco Miele <22mielef@cistercian.org>
2021-09-16 11:07:12 -0500

    new code

--- Exercises/src/Course.java
+++ Exercises/src/Course.java
@@ -1,4 +1,36 @@
-public class Course {
-    int id;
-    String name;
+import java.util.HashSet;
+
+class Course{
+    static int nextId = 0;
+    final int id;
+    final String name;
+    final HashSet<Student> registeredStudents = 
+        new HashSet<Student>();
+    
+    Course(String n){
+        id = nextId;
+        nextId++;
+
+        name = n;
+    }
+    
+    String getName(){
+        return name;
+    }
+
+    void registerStudent(Student s){
+        registeredStudents.add(s);
+    }
+
+    void unregisterStudent(Student s){
+        registeredStudents.remove(s);
+    }
+
+    HashSet<Student> registerStudents(){
+        return registeredStudents;
+    }
+
+    int nrOfRegisteredStudents(){
+        return registeredStudents.size();
+    }
 }
