d350bdce7d7c8509d796bec2cc9421ac07323a5a
Franco Miele <22mielef@cistercian.org>
2021-09-17 14:30:11 -0500

    finished code

--- Exercises/src/Student.java
+++ Exercises/src/Student.java
@@ -1,30 +1,29 @@
-public class Student {
-    int id;
-    String firstName;
-    String lastName;
-    int birthYear, birthMonth, birthDay;
+class Student{
+    static int nextId = 0;
 
-    boolean isBirthday() {
-        //Return true is it's the student's birthday today.
-        return false;
+    final int id;
+    final String firstName, lastName;
+
+    Student(String fn, String ln){
+        id = nextId;
+        nextId++;
+
+        firstName = fn;
+        lastName = ln;
     }
 
-    void giveWarning(boolean isFinalWarning) {
-        //You should study harder!
+    String getfirstName(){
+        return firstName;
     }
 
-    int numberOfFriends() {
-        //Return the number of friends the student has.
-        return 0;
-     }
-     public static void main(String[] args) {
-        Student firstStudent = new Student();
-        Student secondStudent = new Student ();
-        firstStudent.id = 1;
-        firstStudent.firstName = "Marc the Narc";
-        secondStudent.id = 2;
-        secondStudent.firstName = "Sophie (Julius 'girlfriend')";
-    
-        System.out.println("The student object referred to by the variable secondStudent has the first name: " + secondStudent.firstName);
+    String getLastName(){
+        return lastName;
     }
+    void registerForCourse(Course c){
+        c.registerStudent(this);
     }
+
+    void unregisterforCourse(Course c){
+        c.unregisterStudent(this);
+    }
+}
