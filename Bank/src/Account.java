11b03e9a4b5e726350e6e5aedbdf10c5553bc0e5
Franco Miele <22mielef@cistercian.org>
2021-10-05 11:18:05 -0500

    fixed AccountManager

--- Bank/src/Account.java
+++ Bank/src/Account.java
@@ -1,12 +1,52 @@
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
-private class Account {
-    String name;
-    BigDecimal amount;
+public class Account {
+    private String name;
+    private BigDecimal amount;
 
-    private Account(String acctName, String startAmount){
-        this.name = acctName;
-        this.amount = new BigDecimal(startAmount);
-        this.amount.setScale(2, BigDecimal.ROUND_HALF_UP);
+    public Account(String acctName, String startAmount){
+        setName(acctName);
+        setAmount(startAmount);
+        amount.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public BigDecimal getAmount(){
+        return this.amount;
+    }
+
+    public void setName(String newName){
+        String pattern = "^[a-zA-Z0-9]*$";
+        if (newName.matches(pattern)){
+            this.name = newName;
+        }
+    }
+
+    private void setAmount(String newAmount){
+        this.amount = new BigDecimal(newAmount);
+    }
+
+    public void withdraw(String withdraw) throws IllegalArgumentException{
+        BigDecimal desiredAmount = new BigDecimal(withdraw);
+        if (desiredAmount.compareTo(BigDecimal.ZERO) < 0){
+            throw new IllegalArgumentException();
+        }
+        if (amount.compareTo(desiredAmount) < 0){
+            throw new IllegalArgumentException();
+        }
+        this.amount = this.amount.subtract(desiredAmount);
+    }
+
+    public void deposit(String deposit) throws IllegalArgumentException{
+        BigDecimal desiredAmount = new BigDecimal(deposit);
+        if(desiredAmount.compareTo(BigDecimal.ZERO) < 0){
+            throw new IllegalArgumentException();
+        }
+
+        this.amount.add(desiredAmount);
     }
 }
--- Bank/src/AccountManager.java
+++ Bank/src/AccountManager.java
@@ -4,32 +4,15 @@ public class AccountManager{
 
     public static void main(String[] args){
         Account myAccount = new Account("FirstAccount","10.00");
-        System.out.println("Account Created: " + myAccount.name);
-        System.out.println("Balance: " + myAccount.amount);
-        myAccount.amount = new BigDecimal("-15.00");
-        System.out.println("New Balance: " + myAccount.amount);
-    }
-       
-    public String getName(){
-        return this.name;
-    }
+        System.out.println("Account Created: " + myAccount.getName());
+        System.out.println("Balance: " + myAccount.getAmount());
 
-    public BigDecimal getAmount(){
-        return this.amount;
-    }
-    
-    public void setName(String newName){
-        this.name = newName;
-    }
-
-    public void setName(String newName){
-        String pattern = "^[a-zA-Z0-9]*$";
-        if (newName.matches(pattern)){
-            this.name = newName;
-        }
-    }
-
-    private void setAmount(String newAmount){
-        
+            try{
+                myAccount.withdraw("20.00");
+            } catch (IllegalArgumentException e){
+                System.out.println("Invalid Withdraw");
+            } finally {
+                System.out.println("New Balance: " + myAccount.getAmount());
+            }
     }
 }
