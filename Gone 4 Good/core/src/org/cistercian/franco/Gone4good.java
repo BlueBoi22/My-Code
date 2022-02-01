ce517436dea64b1e79f94b1704663da1e3e7c4a4
Franco Miele <22mielef@cistercian.org>
2022-02-01 11:15:38 -0600

    worked on background

--- Gone 4 Good/core/src/org/cistercian/franco/Gone4good.java
+++ Gone 4 Good/core/src/org/cistercian/franco/Gone4good.java
@@ -15,9 +15,11 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class Gone4good extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture items;
+	Texture background;
 	TextureRegion billHud;
 	TextureRegion billHead;
 	TextureRegion billStanding;
+	TextureRegion corner;
 	Animation billWalking;
 	Animation billShootingStart;
 	Animation billShootWalk;
@@ -29,32 +31,37 @@ public class Gone4good extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		items = new Texture("Gone 4 Good.png");
+		background = new Texture("Background.png");
 		billHud = new TextureRegion(items, 0, 0, 184,184);
 		billHead = new TextureRegion(items, 184, 0, 184, 184);
 		billStanding = new TextureRegion(items, 550, 0, 184, 184);
+		corner = new TextureRegion(background, 690, 690, 460, 460);
 		billWalking = new Animation(0.2f, new TextureRegion(items, 550, 0, 184, 184), new TextureRegion(items, 736, 0, 184, 184));
 		billShootingStart = new Animation(.02f, new TextureRegion(items, 0, 184, 184, 184), new TextureRegion(items, 184, 184, 184, 184), new TextureRegion(items, 368, 184, 184, 184), new TextureRegion(items, 552, 184, 184, 184));
 		billShootContinue = new Animation(.02f, new TextureRegion(items, 368, 184, 184, 184), new TextureRegion(items, 552, 184, 184, 184));
 		billShootWalk = new Animation(0.2f, new TextureRegion(items, 736, 184, 184, 184), new TextureRegion(items, 0, 368, 184, 184), new TextureRegion(items, 184, 368, 184, 184), new TextureRegion(items, 184, 552, 184, 184));
 	}
 
+	
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(billStanding, x, y);
 		batch.draw(billHud, 0, 0);
+		batch.draw(corner, 10, 60);
+		if (Gdx.input.isKeyPressed(Input.Keys.W)){
+			y += 10;
+		}
 		batch.draw(billHead, x-50, y+83);
 		batch.end();
-		if (Gdx.input.isKeyPressed(Input.Keys.W)){
-		y += 10;
 	}
-}
 
 
 	@Override
 	public void dispose () {
 		batch.dispose();
 		items.dispose();
+		background.dispose();
 	}
 }
